013ff0a82bf4a89afa160044ac643f927d47e708
Doy Kim <kimdee.dev@gmail.com>
2023-10-01 15:37:43 +0900

    setting: Update Tuist Configuration
    호환가능한 Xcode 버전과 Swift 버전을 Config.swift 에 추가

--- Tuist/Config.swift
+++ Tuist/Config.swift
@@ -1,6 +1,8 @@
 import ProjectDescription
 
 let config = Config(
+    compatibleXcodeVersions: ["14.1"],
+    swiftVersion: "5.7.0",
     plugins: [
         .local(path: .relativeToManifest("../../Plugins/SimpleWeight")),
     ]
